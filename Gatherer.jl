a8043284113cc846be65cb7009b0deae429ac5b7
mantzaris <avmantzaris@gmail.com>
2017-04-17 00:09:51 -0400

    now the function can compute the thresholds for each window of years where the country num is taken from the csvs

--- Gatherer.jl
+++ Gatherer.jl
@@ -1,4 +1,4 @@
-function Gatherer(stYr = 1975,endYr = 1975)
+function Gatherer(stYr = 1975,endYr = 1980,windowSize = 5)
 
 #load the data and add to a dictionary the number of countries in the year range
 countryYearsNum = Dict{Integer,Integer}()
@@ -24,13 +24,38 @@ if( endYr < stYr || stYr < 1975 || endYr > 2003 )
     print(string("year range improperly set, for the Gatherer approach end year must be greater than start and the smallest year is ",1975," and largest ",2003," with smallest first"))
     return
 end
+if( (stYr+windowSize) > endYr)
+    print("not enough years between start and end for analysis")
+    return
+end
 
+windowConf = Dict() #the threshold for significance in each year of window
+yr = stYr
+while( (yr+windowSize) <= endYr )
+    conf5perc = GathererWindow(yr,yr+windowSize,countryYearsNum)
+    yr = yr + windowSize
+    windowConf[string(yr-windowSize,"-",yr)] = conf5perc
+end
+
+print(windowConf)
+
+
+end
+
+
+
+
+
+
+
+
+function GathererWindow(stYr,endYr,countryYearsNum)
 
 SCORES = [12,10,8,7,6,5,4,3,2,1]
 
 AVG_SIMULATION = []
-iterNum = 10000
-confInd5perc = floor(Int,0.05*iterNum)
+iterNum = 250
+confInd5perc = max(1,floor(Int,0.05*iterNum))
 
 for ii = 1:iterNum
     ONE_SIMULATION = []
@@ -48,13 +73,11 @@ for ii = 1:iterNum
     end
     avgSim = mean(ONE_SIMULATION)
     append!(AVG_SIMULATION,[avgSim])
-
+    
 end
 
 sortedAVG_SIMULATION = sort(AVG_SIMULATION,rev=true)
 conf5perc = sortedAVG_SIMULATION[confInd5perc]
-
-println(conf5perc)
 
 return conf5perc
 
