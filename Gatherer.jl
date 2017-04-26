8335c81220c6de920fbb2fcd97476a3300cec43c
mantzaris <avmantzaris@gmail.com>
2017-04-26 15:02:47 -0400

    the Gatherer approach works for the 1975-2003 years by returning the dictionary of country name pairings

--- Gatherer.jl
+++ Gatherer.jl
@@ -2,15 +2,15 @@ function Gatherer(stYr = 1975,endYr = 1980,windowSize = 5)
 
 #load the data and add to a dictionary the number of countries in the year range
 countryYearsNum = Dict{Integer,Integer}()
-resultsFile = readdir("./data/")
+resultsFile = readdir("./dataTables/")
 yrMin = 100000
 yrMax = -1
 for rf in resultsFile
-    fileTmp = open(string("./data/",rf))
+    fileTmp = open(string("./dataTables/",rf))
     linesTmp = readlines(fileTmp)         #readfile lines
-    countryNumTmp = length(split(linesTmp[1],","))
+    countryNumTmp = length(split(linesTmp[1],",")) - 1#COLUMN NAME LIST
     yrTmp = parse(Int,((split(rf,"."))[1]))
-    countryYearsNum[yrTmp] = countryNumTmp
+    countryYearsNum[yrTmp] = countryNumTmp #NUM that receives votes
     close(fileTmp)
     if(yrTmp < yrMin)
         yrMin = yrTmp
@@ -19,6 +19,7 @@ for rf in resultsFile
         yrMax = yrTmp
     end
 end
+
 #sanity check input years
 if( endYr < stYr || stYr < 1975 || endYr > 2003 )
     print(string("year range improperly set, for the Gatherer approach end year must be greater than start and the smallest year is ",1975," and largest ",2003," with smallest first"))
@@ -41,15 +42,54 @@ end
 #MATRIX WINDOW AVGS
 
 print(windowConf)
-avgWindowScore(stYr,endYr,windowSize)
-#return windowConf
-#RESULTS
+winDicts = windowScores(stYr,endYr,windowSize)
+print(winDicts)
 
+collusionDict = countryCollusion(stYr,endYr,windowSize,windowConf,winDicts)
+
+print(collusionDict)
 end
 
 
+
+function countryCollusion(stYr,endYr,windowSize,windowConf,winDicts)
+#FROM THE THRESHOLDS DETERMINE THE COUNTRY PAIRS OF COLLUSION
+
+    collusionDict = Dict()
+    yr = stYr
+    while( (yr+windowSize) <= endYr )
+
+	threshold = windowConf["$(yr)-$(yr + windowSize)"]
+
+	cntryNames = winDicts["$(yr)-$(yr + windowSize)"]["countries"]
+	scoremat = winDicts["$(yr)-$(yr + windowSize)"]["scoremat"]
+	scorematAVG = scoremat * (1/(windowSize+1))
+	
+	collusionDict["$(yr)-$(yr + windowSize)"] = []
+	for ii=1:(size(scoremat)[1])
+	    for jj=1:(size(scoremat)[2])
+		if(scorematAVG[ii,jj] >= threshold)
+		    c1 = cntryNames[ii]
+		    c2 = cntryNames[jj]
+		    prev = collusionDict["$(yr)-$(yr + windowSize)"]
+		    
+		    new = append!(prev, ["$(c1)-$(c2)"])
+		    collusionDict["$(yr)-$(yr + windowSize)"] = new
+		end
+	    end
+	end
+	yr = yr + windowSize
+    end
+
+return collusionDict
+end
+
+
+
+
+
 #FOR EACH TIME WINDOW
-function avgWindowScore(stYr,endYr,windowSize)
+function windowScores(stYr,endYr,windowSize)
 
 #1-DICTIONARY FOR EACH WINDOW INTERVAL
 winDict = Dict()
@@ -62,20 +102,23 @@ while( (yr+windowSize) <= endYr )
 end
 
 #2-FILL DICTIONARY WITH ALL POSSIBLE COUNTRY PAIRS IN THOSE YEARS IT COVERS
-resultsFile = readdir("./data/")
+resultsFile = readdir("./dataTables/")
 namesDict = Dict()
 scoresDict = Dict()
 for rf in resultsFile   
-    yrTmp = parse(Int,((split(rf,"."))[1]))
-    fileTmp = open(string("./data/",rf))                     #each file pipe
+    yrTmp = parse(Int,((split(rf,"."))[1]))   
+    fileTmp = open(string("./dataTables/",rf))                     #each file pipe
     linesTmp = readlines(fileTmp)                            #read each file lines
-    namesDict[yrTmp] = split(linesTmp[1],r",|\n",keep=false) #store the name list
+    tmp = split(linesTmp[1],r",|\n",keep=false) #store the name list
+    splice!(tmp,1) #remove first
+    namesDict[yrTmp] = tmp
     nameNum = length(namesDict[yrTmp])    
     rowNumMat = length(linesTmp)-1
     colNumMat = nameNum
     tmp2 = zeros(rowNumMat,colNumMat)
     for ii=2:length(linesTmp)
-	tmp1 = split(linesTmp[ii],r",|\n",keep=false)	
+	tmp1 = split(linesTmp[ii],r",|\n",keep=false)
+	splice!(tmp1,1) #remove first
 	tmp2[ii-1,:] = [parse(Int,tmp1[jj]) for jj in 1:length(tmp1)]	#each row of the matrix/data     
     end  	    	     
     scoresDict[string(yrTmp)] = tmp2     
@@ -93,8 +136,6 @@ while( (yr+windowSize) <= endYr )
     end
     yr = yr + windowSize
 end
-print("--xxx'winDict'xxx--")
-print(winDict)
 
 #3-ACCUMULATE POINTS OF EACH PARTICULAR COUNTRY TOWARDS ANOTHER DIFFERENT COUNTRY
 yr = stYr
@@ -108,13 +149,7 @@ while( (yr+windowSize) <= endYr )
         matPrev = winDict["$(yr)-$(yr+windowSize)"]["scoremat"]
 	matNew  = zeros(length(matPrev[:,1]),length(matPrev[1,:]))
         namesTotal = winDict["$(yr)-$(yr+windowSize)"]["countries"]	
-	yrScoresMat = scoresDict[string(yr+winInd)]#the matrix for a PARTICULAR YEAR (might not be nxn)
-	#=
-	print("\n--- year---$(yr+winInd)")
-	print("\n ---'matprev'---");print(matPrev)	
-	print("\n ---'yrScoresMat'---");print(yrScoresMat)
-	=#
-	
+	yrScoresMat = scoresDict[string(yr+winInd)]#the matrix for a PARTICULAR YEAR (might not be nxn)		
         yrNames = namesDict[yr+winInd]        
 	       			
 	for rowNum=1:length(yrScoresMat[:,1])#change the year nameInds to the total window Inds of names
@@ -126,21 +161,15 @@ while( (yr+windowSize) <= endYr )
 	    rowNumNew = find([namesTotal[ii] == yrNames[rowNum] for ii in 1:length(namesTotal)])
 	    matNew[rowNumNew,newIndsMat] = yrScoresMat[rowNum,find(yrScoresMat[rowNum,:])]	    
 	end				
-	#=
-	print("\n ---'matNew'---");print(matNew)
-	print("\n ---matNew+matPrev---\n");print(matNew+matPrev)
-	print(winDict["$(yr)-$(yr+windowSize)"]["countries"])
-	=#	
+	
         winDict["$(yr)-$(yr+windowSize)"]["scoremat"] = matPrev + matNew
 		
         winInd = winInd + 1
     end
     yr = yr + windowSize
 end
-print("\n __---winDict__--")
-print(winDict)
-return winDict
 
+return winDict
 
 end
 
