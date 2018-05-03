7da4068287ea7091f9d9143c8d27cf8ed7964d97
mantzaris <avmantzaris@gmail.com>
2018-05-03 16:00:51 -0400

    finished the .dt to csv for the total collusion

--- dot2csvNetTotalCollusion.jl
+++ dot2csvNetTotalCollusion.jl
@@ -1,16 +1,17 @@
 
 function dot2csvTotalCollusion(filename)
-    
+    matCntryEdges = []
     
     fileTmp = open(filename)
     linesTmp = readlines(fileTmp)
     linesStr = linesTmp[1]
-#    println(linesStr)
-#    println(typeof(linesStr))
+    #    println(linesStr)
+    #    println(typeof(linesStr))
     splits1 = split(linesStr,";")
     for strFrag in splits1
 
         if(contains(strFrag,"penwidth"))
+            
             println(strFrag)
             c1end = search(strFrag,"-")
             c1end = collect(c1end)[1]-1
@@ -32,32 +33,41 @@ function dot2csvTotalCollusion(filename)
             finalEnd = collect(finalEnd)[1]-1
             pWmult = parse(Int,strFrag[pW2end:finalEnd])
             println(pWmult)
-        end
+            edgeWeight = pWmult/pW
+            println(edgeWeight)
+            if(isempty(matCntryEdges))
+                matCntryEdges = [c1 c2 edgeWeight]
+            else
+                matCntryEdges = vcat(matCntryEdges,[c1 c2 edgeWeight])
+            end
 
-    end
+        end
         
+       
+    end
+    println(matCntryEdges)
+    writedlm(string(filename,".csv"),matCntryEdges,',')
 end
 
 filename = "networkTotalCollusion:1977-2017windowSize5.dot"
 dot2csvTotalCollusion(filename)
 
-
-#=
-resultsFile = readdir("./dataTables/")
+    #=
+    resultsFile = readdir("./dataTables/")
     yrMin = 100000
     yrMax = -1
     for rf in resultsFile
-        fileTmp = open(string("./dataTables/",rf))
-        linesTmp = readlines(fileTmp)         #readfile lines
-        yrTmp = parse(Int,((split(rf,"."))[1]))
-        countryNumTmp = length(split(linesTmp[1],",")) - 1#COLUMN NAME LIST (still the same regarding large set mapping to small set in cols) OF WHO CAN RECEIVE VOTES
-        countryYearsNum[yrTmp] = countryNumTmp #NUM that receives votes
-        close(fileTmp)
-        if(yrTmp < yrMin)
-            yrMin = yrTmp
-        end
-        if(yrTmp > yrMax)
-            yrMax = yrTmp
-        end
+    fileTmp = open(string("./dataTables/",rf))
+    linesTmp = readlines(fileTmp)         #readfile lines
+    yrTmp = parse(Int,((split(rf,"."))[1]))
+    countryNumTmp = length(split(linesTmp[1],",")) - 1#COLUMN NAME LIST (still the same regarding large set mapping to small set in cols) OF WHO CAN RECEIVE VOTES
+    countryYearsNum[yrTmp] = countryNumTmp #NUM that receives votes
+    close(fileTmp)
+    if(yrTmp < yrMin)
+    yrMin = yrTmp
     end
-=#
+    if(yrTmp > yrMax)
+    yrMax = yrTmp
+    end
+    end
+    =#
