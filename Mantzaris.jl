3b70dc0facee2753dae86dade8a911c93df6395e
mantzaris <avmantzaris@gmail.com>
2018-05-04 00:47:33 -0400

    not sure what I changed in this file, but should be minor

--- Mantzaris.jl
+++ Mantzaris.jl
@@ -217,7 +217,7 @@ end
 #THRESHOLD FOR EACH TIME WINDOW; looking at each year in the range-> for each year draw a hypothetical score -> from the applicable voting paradigm -> accumulate the score (I will simulate each year independently from stYr:endYr choosing the appropriate scheme each time
 function scoreSim(stYr,endYr,countryYearsNum)
     AVG_SIMULATION = []
-    iterNum = 2500
+    iterNum = 3000
     confInd5perc = max(1,floor(Int,0.05*iterNum))
     confInd10perc = max(1,floor(Int,0.1*iterNum))
     for ii = 1:iterNum
