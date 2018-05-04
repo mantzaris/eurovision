791ac30a883c64b7b026c3e9f3cf91f0578ae4b4
mantzaris <avmantzaris@gmail.com>
2018-05-03 20:00:07 -0400

    now working on the files for the oneway and two way

--- dot2csvNetTotalCollusion.jl
+++ dot2csvNetTotalCollusion.jl
@@ -4,53 +4,96 @@ function dot2csvTotalCollusion(filename)
     
     fileTmp = open(filename)
     linesTmp = readlines(fileTmp)
-    linesStr = linesTmp[1]
-    #    println(linesStr)
-    #    println(typeof(linesStr))
+    linesStr = linesTmp[1]#println(linesStr)#println(typeof(linesStr))
     splits1 = split(linesStr,";")
     for strFrag in splits1
 
-        if(contains(strFrag,"penwidth"))
-            
-            println(strFrag)
+        if(contains(strFrag,"penwidth"))#println(strFrag)
             c1end = search(strFrag,"-")
             c1end = collect(c1end)[1]-1
-            c1 = strFrag[1:c1end]
-            println(c1)
+            c1 = strFrag[1:c1end]#println(c1)
             c2start = search(strFrag,">")
             c2end = search(strFrag," [")
             c2start = collect(c2start)[1]+1
             c2end = collect(c2end)[1]-1
-            c2 = strFrag[c2start:c2end]
-            println(c2)
+            c2 = strFrag[c2start:c2end]#println(c2)
             pW1end = search(strFrag,"penwidth=")
             pW1end = collect(pW1end)[end]+1
-            pW = parse(Int,strFrag[pW1end])
-            println(pW)
+            pW = parse(Int,strFrag[pW1end])#println(pW)
             pW2end = search(strFrag,"penwidth=",collect(search(strFrag,"penwidth="))[end]+1)
             finalEnd = search(strFrag,"]")
             pW2end = collect(pW2end)[end]+1
             finalEnd = collect(finalEnd)[1]-1
-            pWmult = parse(Int,strFrag[pW2end:finalEnd])
-            println(pWmult)
-            edgeWeight = pWmult/pW
-            println(edgeWeight)
+            pWmult = parse(Int,strFrag[pW2end:finalEnd])#println(pWmult)
+            edgeWeight = pWmult/pW#println(edgeWeight)
             if(isempty(matCntryEdges))
                 matCntryEdges = [c1 c2 edgeWeight]
             else
                 matCntryEdges = vcat(matCntryEdges,[c1 c2 edgeWeight])
             end
-
-        end
-        
-       
-    end
-    println(matCntryEdges)
-    writedlm(string(filename,".csv"),matCntryEdges,',')
+        end               
+    end    
+    writedlm(string(filename,".csv"),matCntryEdges,',')#println(matCntryEdges)
 end
 
-filename = "networkTotalCollusion:1977-2017windowSize5.dot"
-dot2csvTotalCollusion(filename)
+
+function dot2csvOneWayTwoWay(filename)
+    matCntryEdges = []
+    
+    fileTmp = open(filename)
+    linesTmp = readlines(fileTmp)
+    linesStr = linesTmp[1]#
+    println(linesStr)#
+    println(typeof(linesStr))
+    splits1 = split(linesStr,";")
+    
+    
+    for strFrag in splits1
+        
+        if(contains(strFrag,"->"))#
+            c1end = search(strFrag,"-")
+            c1end = collect(c1end)[1]-1
+            c1 = strFrag[1:c1end]#println(c1)
+            
+            c2start = search(strFrag,">")
+            c2start = collect(c2start)[1]+1
+            c2end = -1
+            way = "oneWay"
+            if(isempty(collect(search(strFrag," ["))))
+                c2end = length(strFrag)#collect(c2end)[1]-1                
+            else
+                c2end = search(strFrag," [")
+                c2end = collect(c2end)[1]-1
+                way = "twoWay"
+            end                    
+            c2 = strFrag[c2start:c2end]#
+            if(isempty(matCntryEdges))
+                matCntryEdges = [c1 c2 way]
+                if(way == "twoWay")
+                    matCntryEdges = vcat(matCntryEdges,[c2 c1 way])
+                end
+            else
+                matCntryEdges = vcat(matCntryEdges,[c1 c2 way])
+                if(way == "twoWay")
+                    matCntryEdges = vcat(matCntryEdges,[c2 c1 way])
+                end
+            end            
+        end               
+    end    
+    writedlm(string(filename,".csv"),matCntryEdges,',')#println(matCntryEdges)
+end
+
+filename = "network:1977-1982.dot"
+dot2csvOneWayTwoWay(filename)
+
+#filename = "networkTotalCollusion:1977-2017windowSize5.dot"
+#dot2csvTotalCollusion(filename)
+
+
+
+
+
+
 
     #=
     resultsFile = readdir("./dataTables/")
